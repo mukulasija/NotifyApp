39588fb798c336795b6aafebbcfb53d634152051
mukulasija <lit2020010@iiitl.ac.in>
2021-10-09 17:53:09 +0530

    tests passed on android 9

--- app/src/main/java/com/example/notifyapp/model/CallReciever.kt
+++ app/src/main/java/com/example/notifyapp/model/CallReciever.kt
@@ -16,20 +16,45 @@ class CallReciever : BroadcastReceiver() {
 var oldstate : String?  = TelephonyManager.EXTRA_STATE_IDLE
         if(p1!!.getStringExtra(TelephonyManager.EXTRA_STATE)==TelephonyManager.EXTRA_STATE_OFFHOOK){
            showToastMsg(p0!!,"Phone call is started")
-            oldstate = TelephonyManager.EXTRA_STATE
+//            oldstate = TelephonyManager.EXTRA_STATE
         }
         else
             if(p1!!.getStringExtra(TelephonyManager.EXTRA_STATE)==TelephonyManager.EXTRA_STATE_IDLE){
 
                 showToastMsg(p0!!,"Phone call Ended")
-                oldstate= TelephonyManager.EXTRA_STATE_IDLE
+//                oldstate= TelephonyManager.EXTRA_STATE_IDLE
             }
         else
-                if(p1!!.getStringExtra(TelephonyManager.EXTRA_STATE)==TelephonyManager.EXTRA_STATE_RINGING && oldstate!!.compareTo(TelephonyManager.EXTRA_STATE_IDLE)==0){
+//                    && oldstate!!.compareTo(TelephonyManager.EXTRA_STATE_IDLE)==0
+            {
+//                val action: String = p1.getAction() ?: return
+//
+//                if (action != "android.intent.action.PHONE_STATE") return
+//                var curState = p1.getStringExtra(TelephonyManager.EXTRA_STATE)
+//                if(TelephonyManager.EXTRA_STATE_RINGING.equals(curState)&& TelephonyManager.EXTRA_STATE_IDLE.equals(oldstate)){
+//                    val incNum : String = p1.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)!!
+//                }
+//                oldstate = curState
+//
+//                String curState = aIntent.getStringExtra(TelephonyManager.EXTRA_STATE);
+//
+//                if ((TelephonyManager.EXTRA_STATE_RINGING.equals(curState))
+//                    &&(TelephonyManager.EXTRA_STATE_IDLE.equals(oldState)))){
+//                String incNumber = aIntent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+//
+//                // do something here
+//            }
+//                oldState=curState;
+
+                if (p1!!.getStringExtra(TelephonyManager.EXTRA_STATE) == TelephonyManager.EXTRA_STATE_RINGING) {
+
                     val phoneno = p1.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
-                    showToastMsg(p0!!,"Incomming call"+phoneno)
-                    oldstate = p1.getStringExtra(TelephonyManager.EXTRA_STATE)
+                    if(!phoneno.equals("") && phoneno!=null && !phoneno.isEmpty()) {
+                        showToastMsg(p0!!, "Incomming call" + phoneno)
+                    }
+//                    oldstate = p1.getStringExtra(TelephonyManager.EXTRA_STATE)
                 }
+            }
     }
 
     fun showToastMsg(c: Context, msg: String)
